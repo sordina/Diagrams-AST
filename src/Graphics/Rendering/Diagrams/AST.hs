e284bfb76caf4089a2c3949a7abf495a7e787f27
Lyndon Maydwell <maydwell@gmail.com>
2011-05-31 17:41:29 +0800

    Adding more data types

--- src/Graphics/Rendering/Diagrams/AST.hs
+++ src/Graphics/Rendering/Diagrams/AST.hs
@@ -1,18 +1,34 @@
 {-# LANGUAGE NoMonomorphismRestriction, FlexibleContexts #-}
 
 module Graphics.Rendering.Diagrams.AST (
+  -- Functions
   outputImage,
   runImage,
-  Image(..),
-  Modifier(..),
-  Combiner(..),
-  Shape(..),
-  ColorData(..)
+
+  -- Data Types
+  Image     (..),
+  Modifier  (..),
+  Combiner  (..),
+  Path      (..),
+  Shape     (..),
+  ColorData (..),
+  Fill      (..),
+  NodeStyle (..),
+
+  -- Newtypes
+  Offset (..),
+  Point  (..),
+  Angle  (..)
   )
 where
 
-import Diagrams.Prelude hiding (LineColor, LineWidth, Dashing, Point, Path)
-import Diagrams.Backend.Cairo
+-- Diagram Imports
+import qualified Diagrams.Prelude as D
+import           Diagrams.Prelude ((|||), (===))
+import           Diagrams.Backend.Cairo
+
+-- Data Imports
+import Data.Monoid
 import Data.List (foldl')
 
 --- Data Types
@@ -39,27 +55,32 @@ data Combiner = Atop   Image Image
               | Vertical   [Image] deriving (Show, Eq, Ord)
 
 data Path = Offsets [Offset]
-          | Points  [Point] deriving (Show, Eq, Ord)
-
-newtype Offset = Offset Double deriving (Show, Eq, Ord)
-newtype Point  = Point  Double deriving (Show, Eq, Ord)
+          | Points  [Point]
+          | Arc      Angle  deriving (Show, Eq, Ord)
 
 data Shape = Circle
-           | Path Fill Path deriving (Show, Eq, Ord)
+           | Path Fill NodeStyle Path deriving (Show, Eq, Ord)
 
 data ColorData = ColorData Double Double Double Double deriving (Show, Eq, Ord)
 
-data Fill = Join
+data Fill = Joined
           | Open deriving (Show, Eq, Ord)
+
+data NodeStyle = Smooth
+               | Sharp deriving (Show, Eq, Ord)
+
+newtype Offset = Offset Double deriving (Show, Eq, Ord)
+newtype Point  = Point  Double deriving (Show, Eq, Ord)
+newtype Angle  = Angle  Double deriving (Show, Eq, Ord)
 
 --- Instances
 
-instance Color ColorData where
+instance D.Color ColorData where
   colorToRGBA (ColorData r g b a) = (r, g, b, a)
 
 ---- Run ADT Functions
 
-outputImage name width height image = renderDia Cairo (CairoOptions name (PNG (width, height))) (runImage image)
+outputImage name width height image = D.renderDia Cairo (CairoOptions name (PNG (width, height))) (runImage image)
 
 --- Main runner
 
@@ -69,21 +90,21 @@ runImage (Combiner c)   = runCombiner c
 
 --- Internal runners
 
-runCombiner (Atop   l r)   = runImage l `atop` runImage r
+runCombiner (Atop   l r)   = runImage l `D.atop` runImage r
 runCombiner (NextTo l r)   = runImage l ||| runImage r
 runCombiner (Above  t b)   = runImage t === runImage b
 runCombiner (Layers     l) = mconcat . map runImage $ l
-runCombiner (Horizontal l) = hcat (map runImage l)
-runCombiner (Vertical   l) = vcat (map runImage l)
+runCombiner (Horizontal l) = D.hcat (map runImage l)
+runCombiner (Vertical   l) = D.vcat (map runImage l)
 
-runShape Circle = circle
+runShape Circle = D.circle
 
-runModifier (Foreground c)  = fillColor c
-runModifier (LineColor  c)  = lineColor c
-runModifier (LineWidth  w)  = lw w
-runModifier (Dashing  a w)  = dashing a w
-runModifier (Translate x y) = translate (x, y)
-runModifier (Scale x y)     = scaleX x . scaleY y
-runModifier (Pad r)         = pad r
+runModifier (Foreground c)  = D.fillColor c
+runModifier (LineColor  c)  = D.lineColor c
+runModifier (LineWidth  w)  = D.lw w
+runModifier (Dashing  a w)  = D.dashing a w
+runModifier (Translate x y) = D.translate (x, y)
+runModifier (Scale x y)     = D.scaleX x . D.scaleY y
+runModifier (Pad r)         = D.pad r
 runModifier (Changes l)     = foldl' (.) id . map runModifier $ l
-runModifier  Freeze         = freeze
+runModifier  Freeze         = D.freeze
