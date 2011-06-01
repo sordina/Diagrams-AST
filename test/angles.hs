965d12b9f4e861bdb6a613978a4d7c4bf5b5a704
Lyndon Maydwell <maydwell@gmail.com>
2011-06-01 22:10:04 +0800

    More crazyness. Time for a new example I think.

--- test/angles.hs
+++ test/angles.hs
@@ -2,9 +2,13 @@ import Graphics.Rendering.Diagrams.AST
 import System.Random
 
 main = do
+  mapM_ step [1..100]
+
+step n = do
+  print n
   ps <- paths
-  outputImage "../angles.png" 600 600 $
-    Images (Layers ps)
+  outputImage ("../angles_" ++ show n ++ ".png") 600 600 $
+    Modifier (Pad 1.1) $ Images $ Layers (circle : ps)
 
 paths :: IO [Image]
 paths = do
@@ -12,14 +16,11 @@ paths = do
   r2 <- range (-5,5)
   c1 <- range  (0,1)
   c2 <- range  (0,1)
-  return $
-    map shape $
-    zip (zipWith color c1 c2) $
-    take 100 $
-    chunk 4 $
-    zip r1 r2
+  return $ zipWith shape
+    (zipWith color c1 c2)
+    (take 100 $ chunk 5 $ zip r1 r2)
 
-shape (c,l) =
+shape c l =
   Modifier (Changes [Foreground c, LineWidth 0.05]) $
   Shape $
   Path Closed Sharp $
@@ -34,4 +35,6 @@ range b = do
   g <- newStdGen
   return $ randomRs b g
 
-color g e = RAA 1 g e 0.95
+color g e = RAA 1 g e 0.94
+
+circle = Modifier (Scale 30 30) $ Shape Circle
