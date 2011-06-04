1e2d62c97d2b54d346e1bed36d722a633cb5299e
Lyndon Maydwell <maydwell@gmail.com>
2011-06-04 14:01:54 +0800

    Adding helper package for useful functions

---
+++ src/Graphics/Rendering/Diagrams/AST/Helpers.hs
@@ -0,0 +1,10 @@
+module Graphics.Rendering.Diagrams.AST.Helpers (
+    barchart
+  ) where
+
+import Graphics.Rendering.Diagrams.AST
+
+barchart :: ColorData -> [Double] -> Image
+barchart color = Modifier (Changes [Align C, Pad 1.2]) . Images . Horizontal . map bar
+  where
+    bar y = Modifier (Changes [Align B, Foreground color, Scale 1 (5*y)]) (Shape Square)
