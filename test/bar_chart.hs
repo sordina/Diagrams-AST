b22fc561e2c391ba01d6dd0387c275a41e47a58f
Lyndon Maydwell <maydwell@gmail.com>
2011-06-04 13:08:38 +0800

    Making chart easier to see against white background

--- test/bar_chart.hs
+++ test/bar_chart.hs
@@ -1,6 +1,7 @@
 import Graphics.Rendering.Diagrams.AST
 import System.Random
 import Control.Applicative
+import Data.List (sort)
 
 main = do
   heights <- fmap (sort . take 100) $ (zipWith (+)) <$> range (0,10) <*> range (0,10)
@@ -8,8 +9,8 @@ main = do
 
 barchart = Modifier (Changes [Align C, Pad 1.2]) . Images . Horizontal . map bar
 
-bar y = Modifier (Changes [Align B, Foreground white, Scale 1 (5*y)]) (Shape Square)
+bar y = Modifier (Changes [Align B, Foreground blue, Scale 1 (5*y)]) (Shape Square)
 
 range b = fmap (randomRs b) newStdGen
 
-white = RGBA 1 1 1 1
+blue = RGBA 0.2 0.3 1 1
