6abb5a1087726bf27fee9f1c0aa80cb172c9d08f
Lyndon Maydwell <maydwell@gmail.com>
2011-06-06 22:54:11 +0800

    Adding More optimizations

--- src/Graphics/Rendering/Diagrams/AST/Optimize.hs
+++ src/Graphics/Rendering/Diagrams/AST/Optimize.hs
@@ -2,6 +2,7 @@ module Graphics.Rendering.Diagrams.AST.Optimize ( optimize ) where
 
 import Graphics.Rendering.Diagrams.AST
 import Data.Generics.Uniplate.Operations
+import Data.List (sort)
 
 -- Consider using "rewrite" instead of transform (http://community.haskell.org/~ndm/darcs/uniplate/uniplate.htm)
 optimize :: Image -> Image
@@ -16,24 +17,30 @@ o (Modifier (Scale _ 0) _) = Blank
 o (Modifier (Scale 1 1) i) = i
 
 -- Consecutive Scales
-o (Modifier (Scale x y) (Modifier (Scale x' y') i)) =
-  Modifier (Scale (x*x') (y*y')) i
+o (Modifier (Scale x y) (Modifier (Scale x' y') i)) = Modifier (Scale (x*x') (y*y')) i
 
 -- Consecutive Translations
-o (Modifier (Translate x y) (Modifier (Translate x' y') i)) =
-  Modifier (Translate (x*x') (y*y')) i
+o (Modifier (Translate x y) (Modifier (Translate x' y') i)) = Modifier (Translate (x*x') (y*y')) i
+
+o (Modifier (Changes [])  i) = i
+o (Modifier (Changes [c]) i) = Modifier c i
+-- o (Modifier (Changes l)   i) = Modifier (Changes (sort l)) i -- Not concatinating yet
 
 -- Removing Blanks from Combinations
-o (Images (Atop Blank i))  = i
-o (Images (Atop  i Blank)) = i
-o (Images (Above Blank i)) = i
-o (Images (Above i Blank)) = i
-o (Images (Layers []))     = Blank
-o (Images (Layers xs))     = Images $ Layers $ filter (not.blank) xs
-o (Images (Horizontal [])) = Blank
-o (Images (Horizontal xs)) = Images $ Horizontal $ filter (not.blank) xs
-o (Images (Vertical []))   = Blank
-o (Images (Vertical xs))   = Images $ Vertical $ filter (not.blank) xs
+-- Sort lists to allow for transformation concatinations
+o (Images (Atop Blank i))   = i
+o (Images (Atop  i Blank))  = i
+o (Images (Above Blank i))  = i
+o (Images (Above i Blank))  = i
+o (Images (Layers     []))  = Blank
+o (Images (Layers     [i])) = i
+o (Images (Layers     xs))  = Images $ Layers $ filter (not.blank) (sort xs)
+o (Images (Vertical   []))  = Blank
+o (Images (Vertical   [i])) = i
+o (Images (Vertical   xs))  = Images $ Vertical $ filter (not.blank) (sort xs)
+o (Images (Horizontal []))  = Blank
+o (Images (Horizontal [i])) = i
+o (Images (Horizontal xs))  = Images $ Horizontal $ filter (not.blank) (sort xs)
 
 -- etc, etc
 
