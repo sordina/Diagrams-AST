d1a7b7e92356b53402fb55381b2d2f7981ee21fc
Lyndon Maydwell <maydwell@gmail.com>
2011-06-06 22:14:41 +0800

    separating out optimization and helpers

--- src/Graphics/Rendering/Diagrams/AST/Helpers.hs
+++ src/Graphics/Rendering/Diagrams/AST/Helpers.hs
@@ -1,50 +1,6 @@
-module Graphics.Rendering.Diagrams.AST.Helpers (
-    optimize,
-    barchart
-  ) where
+module Graphics.Rendering.Diagrams.AST.Helpers ( barchart) where
 
 import Graphics.Rendering.Diagrams.AST
-import Data.Generics.Uniplate.Operations
-
-
--- Example of an optimization routime using uniplate
-
-optimize :: Image -> Image
-optimize = transform o
-
--- Don't modify blank images
-o (Modifier _ Blank) = Blank
-
--- Leaving the debate about pattern-matching on doubles until later
-o (Modifier (Scale 0 _) _) = Blank
-o (Modifier (Scale _ 0) _) = Blank
-o (Modifier (Scale 1 1) i) = i
-
--- Consecutive Scales
-o (Modifier (Scale x y) (Modifier (Scale x' y') i)) =
-  Modifier (Scale (x*x') (y*y')) i
-
--- Consecutive Translations
-o (Modifier (Translate x y) (Modifier (Translate x' y') i)) =
-  Modifier (Translate (x*x') (y*y')) i
-
--- Removing Blanks from Combinations
-o (Images x) = f x
-  where
-    f (Atop  Blank i)     = i
-    f (Atop  i     Blank) = i
-    f (Above Blank i)     = i
-    f (Above i     Blank) = i
-    f (Layers     []) = Blank
-    f (Horizontal []) = Blank
-    f (Vertical   []) = Blank
--- etc, etc
-
--- Default
-o x = x
-
-
--- Barchart example
 
 barchart :: ColorData -> [Double] -> Image
 barchart color = Modifier (Changes [Align C, Pad 1.2]) . Images . Horizontal . map bar
--- test/recursion.hs
+++ test/recursion.hs
@@ -1,10 +1,10 @@
 import Graphics.Rendering.Diagrams.AST
 
 main     = outputImage "recursion.png" 300 300 pilla
-pilla = Images $ Atop face body
-face =  Modifier (Scale 0.3 0.3) $ Images $ Horizontal [pcirc, pcirc]
-pcirc = Modifier (Foreground black) $ Shape Circle
-body = circles !! 50
+pilla    = Images $ Atop face body
+face     =  Modifier (Scale 0.3 0.3) $ Images $ Horizontal [pcirc, pcirc]
+pcirc    = Modifier (Foreground black) $ Shape Circle
+body     = circles !! 50
 circles  = iterate (Images . Atop circle . modifier) Blank
 circle   = Modifier (Foreground green) $ Shape Circle
 modifier = Modifier $ Changes [Scale 0.9 0.9, Rotate (Fraction 0.1), Translate 0 (-2)]
