f447154bb775dfdaefda07042d23c413f27ec274
Lyndon Maydwell <maydwell@gmail.com>
2011-06-07 04:45:12 +0800

    Fixing doc for angle datatype; refactoring optimization code

--- src/Graphics/Rendering/Diagrams/AST.hs
+++ src/Graphics/Rendering/Diagrams/AST.hs
@@ -19,9 +19,7 @@ module Graphics.Rendering.Diagrams.AST (
   ColorData (..),
   Fill      (..),
   Alignment (..),
-
-  -- Newtypes
-  Angle  (..)
+  Angle     (..)
   )
 where
 
--- src/Graphics/Rendering/Diagrams/AST/Optimize.hs
+++ src/Graphics/Rendering/Diagrams/AST/Optimize.hs
@@ -5,6 +5,7 @@ module Graphics.Rendering.Diagrams.AST.Optimize ( optimize ) where
 
 import Graphics.Rendering.Diagrams.AST
 import Data.Generics.Uniplate.Data
+import Data.Maybe
 
 {- | The function 'optimize' takes an Image and returns an equivilant Image that optimizes the structure of the data.
 
@@ -24,12 +25,12 @@ optimize :: Image -> Image
 optimize = rewrite o
 
 -- Don't modify blank images
-o (Modifier _ Blank) = Just $ Blank
+o (Modifier _ Blank) = Just Blank
 
 -- Leaving the debate about pattern-matching on doubles until later
-o (Modifier (Scale 0 _) _) = Just $ Blank
-o (Modifier (Scale _ 0) _) = Just $ Blank
-o (Modifier (Scale 1 1) i) = Just $ i
+o (Modifier (Scale 0 _) _) = Just Blank
+o (Modifier (Scale _ 0) _) = Just Blank
+o (Modifier (Scale 1 1) i) = Just i
 
 o (Modifier (Rotate 0) i) = Just i
 
@@ -45,32 +46,27 @@ o (Modifier (Translate x y) (Modifier (Translate x' y') i)) = Just $ Modifier (T
 o (Modifier (Rotate a) (Modifier (Rotate a') i)) = Just $ Modifier (Rotate (a+a')) i
 
 -- Sets of changes
-o (Modifier (Changes [])  i) = Just $ i
+o (Modifier (Changes [])  i) = Just i
 o (Modifier (Changes [c]) i) = Just $ Modifier c i
-o (Modifier (Changes l)   i) = g (f (Left l))
+o (Modifier (Changes l)   i) = f l >>= Just . flip Modifier i . Changes
   where
-    g (Right l) = Just $ Modifier (Changes l) i
-    g (Left  l) = Nothing
-
-    f (Left  (Scale     x y : Scale     x' y' : l)) = f $ Right $ Scale     (x*x') (y*y') : h (f $ Left l)
-    f (Left  (Translate x y : Translate x' y' : l)) = f $ Right $ Translate (x+x') (y+y') : h (f $ Left l)
-    f (Left  (Rotate    x   : Rotate    x'    : l)) = f $ Right $ Rotate    (x+x')        : h (f $ Left l)
-    f x = x
-
-    h (Left  l) = l
-    h (Right l) = l
+    f (Rotate    x   : Rotate    x'    : cs) = Just $ g (Rotate    (x+x')        : g cs)
+    f (Scale     x y : Scale     x' y' : cs) = Just $ g (Scale     (x*x') (y*y') : g l)
+    f (Translate x y : Translate x' y' : cs) = Just $ g (Translate (x+x') (y+y') : g l)
+    f _ = Nothing
+    g l = fromMaybe l (f l)
 
 -- Removing Blanks from Combinations
-o (Images (Atop Blank i))   = Just $ i
-o (Images (Atop  i Blank))  = Just $ i
-o (Images (Above Blank i))  = Just $ i
-o (Images (Above i Blank))  = Just $ i
-o (Images (Layers     []))  = Just $ Blank
-o (Images (Layers     [i])) = Just $ i
-o (Images (Vertical   []))  = Just $ Blank
-o (Images (Vertical   [i])) = Just $ i
-o (Images (Horizontal []))  = Just $ Blank
-o (Images (Horizontal [i])) = Just $ i
+o (Images (Atop Blank i))   = Just i
+o (Images (Atop  i Blank))  = Just i
+o (Images (Above Blank i))  = Just i
+o (Images (Above i Blank))  = Just i
+o (Images (Layers     []))  = Just Blank
+o (Images (Layers     [i])) = Just i
+o (Images (Vertical   []))  = Just Blank
+o (Images (Vertical   [i])) = Just i
+o (Images (Horizontal []))  = Just Blank
+o (Images (Horizontal [i])) = Just i
 o (Images (Horizontal xs))
   | any blank xs = Just $ Images $ Horizontal $ filter (not.blank) xs
   | otherwise    = Nothing
